93a96e5c5f3f383fffec42745947ad2465e843c2
Dan Croak <dan@thoughtbot.com>
2012-11-09 14:46:49 -0800

    Hide repos that had no commits this week

--- interaction/markdown_generator.rb
+++ interaction/markdown_generator.rb
@@ -6,11 +6,15 @@ class MarkdownGenerator
   end
 
   def generate
-    markdown = '# This Week in Open Source'
+    markdown = "# This Week in Open Source\n\n"
+    markdown << "Thanks to this week's contributors to "
+    markdown << "[thoughtbot](http://github.com/thoughtbot) projects.\n\n"
 
     repos.each do |repo|
-      markdown << "## [#{repo.name}](#{repo.url})\n"
-      markdown << "#{repo.description}\n\n"
+      if repo.contributors.any?
+        markdown << "## [#{repo.name}](#{repo.url})\n"
+        markdown << "#{repo.description}\n\n"
+      end
 
       repo.contributors.each do |contributor|
         if contributor.avatar_url
