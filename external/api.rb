8ddcdef4366ed508e28909c0b6b2123a29f764a4
Dan Croak <dan@thoughtbot.com>
2012-11-24 10:56:50 -0800

    Remove Api#post

--- external/api.rb
+++ external/api.rb
@@ -8,12 +8,6 @@ class Api
   end
 
   def get(endpoint)
-    response = `curl -s https://#{host}#{endpoint}`
-    JSON.parse(response)
-  end
-
-  def post(endpoint, data)
-    response = `curl -s -d '#{data.to_json}' https://#{host}#{endpoint}`
-    JSON.parse(response)
+    JSON.parse(`curl -s https://#{host}#{endpoint}`)
   end
 end
